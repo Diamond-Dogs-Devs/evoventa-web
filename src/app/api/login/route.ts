b8a2486d9ea7b5ed73cd1f2f3681b894a3cb3eba
Edgar Andrei Olvera <eandrei.olvera@gmail.com>
2026-09-23 22:46:35 -0600

    fix: optmize code

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,67 +3,50 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const body = await req.json();
-    if (body) {
-      const IS_MOVILE = body?.grant === "movile";
-      if (body?.email === undefined || body?.password === undefined) {
-        const res = NextResponse.json(
-          {
-            error: true,
-            messages: "Parametros inválidos",
-          },
-          {
-            status: 400,
-          }
-        );
-        return res;
-      }
-      const authController = new AuthController();
-      const { token, user } = await authController.loginUser(
-        body.email,
-        body.password
-      );
+    const { email, password } = await req.json();
 
-      const res = NextResponse.json(
-        {
-          ok: user,
-          token: token,
-        },
-        {
-          status: 200,
-        }
-      );
-
-      res.cookies.set("access_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-        maxAge: 60 * 60 * 24,
-      });
-
-      return res;
-    } else {
-      const res = NextResponse.json(
+    if (!email || !password) {
+      return NextResponse.json(
         {
           error: true,
-          messages: "Parametros inválidos",
+          messages: "Parámetros inválidos",
         },
-        {
-          status: 400,
-        }
+        { status: 400 }
       );
-      return res;
     }
-  } catch (e) {
-    const res = NextResponse.json(
+
+    const authController = new AuthController();
+
+    const { token, user } = await authController.loginUser(email, password);
+
+    const response = NextResponse.json(
+      {
+        user,
+        token,
+      },
+      { status: 200 }
+    );
+
+    response.cookies.set({
+      name: "access_token",
+      value: token,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Login error:", error);
+
+    return NextResponse.json(
       {
         error: true,
         messages: "Hubo un error al invocar el servicio",
       },
-      {
-        status: 500,
-      }
+      { status: 500 }
     );
-    return res;
   }
 }
